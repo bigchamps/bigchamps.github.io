c9390731f0248dad36c7037022a0395ab21a0eae
Hoang Dang <hoangdang.eng@gmail.com>
2024-02-15 01:36:48 -0500

    home page and hero section

---
+++ public/js/typewrite.js
@@ -0,0 +1,72 @@
+var TxtType = function (el, toRotate, period, keepLast=false) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+    this.keepLast = keepLast
+};
+
+TxtType.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+
+    var fullTxt = this.toRotate[i];
+    
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+
+        if (this.loopNum == (this.toRotate.length -1) && this.keepLast){
+            display_prompt()
+            return
+        }
+ 
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+
+    }
+    if (this.loopNum == this.toRotate.length) {
+        return;
+    }
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+
+};
+
+window.addEventListener("load", (event) => {
+    var elements = document.getElementsByClassName('typewrite');
+    
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        var keeplast = elements[i].getAttribute('keep-last');
+        console.log(elements[i])
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period, keeplast);
+        }
+    }
+  });
+
+function display_prompt(){
+    var prompt = document.getElementById('main-control')
+    prompt.style.visibility = "visible"
+    prompt.classList.add("popInAnimation")
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,58 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-            colors:{
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        fontFamily: {
+            quirlycues: ["Quirlycues", "sans-serif"],
+            ain: ["AiN", "sans-serif"],
+        },
+
+        extend: {
+            colors: {
                 primary: '',
                 secondary: '',
+            },
+            fontSize: {
+                'myname': ['3rem', {
+                    lineHeight: '5rem',
+                    letterSpacing: '-0.01em',
+                    fontWeight: '500',
+                }],
+            },
+            keyframes: {
+                typing: {
+                    "0%": {
+                        width: "0%",
+                        visibility: "hidden"
+                    },
+                    "100%": {
+                        width: "100%"
+                    }
+                },
+                blink: {
+                    "50%": {
+                        borderColor: "transparent"
+                    },
+                    "100%": {
+                        borderColor: "white"
+                    }
+                },
+                wiggle: {
+                    '0%, 100%': { transform: 'rotate(0)' },
+                    '25%': { transform: 'rotate(3deg)' },
+                    '75%': { transform: 'rotate(-3deg)' }
+                  }
+            },
+            animation: {
+                typing: "typing 2s steps(20) infinite alternate, blink .7s infinite",
+                'spin-slow': 'spin 3s linear infinite',
+                blinking: 'blink .2s infinite',
+                'wiggle-2': 'wiggle 1s linear 2'
             }
         },
-	},
-	plugins: [],
+        
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
 }
